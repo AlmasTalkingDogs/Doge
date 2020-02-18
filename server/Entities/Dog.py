59177858d1b1fb11f63c5297a3ea367808e08c05
dhruv <dhruvr4@illinois.edu>
2020-02-17 19:50:05 -0600

    Done

--- server/Entities/Dog.py
+++ server/Entities/Dog.py
@@ -1,12 +1,6 @@
 class Dog:
-    def __init__(self, name='chuchu'):
+    def __init__(self, name='chuchu',id):
         self.name = name
 
-    def __set_name__(self, name):
-        self.name = name
 
-    def __get_name__(self):
-        return self.name
 
-    def __del__(self):
-        del (self)
