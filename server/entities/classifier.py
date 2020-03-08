cacef561c7c4c479eae04691dc76e059ea91fa23
asdhruv2 <asdhruv2@illinois.edu>
2020-03-08 14:12:19 -0500

    fixed classifier:

--- server/entities/classifier.py
+++ server/entities/classifier.py
@@ -1,11 +1,10 @@
 class Classifier():
 
-    def _init_(self):
-    	self.name = "sampleName"
-    	self.sample_data = None
-    def classify(self, sample_data):
-    	self.sample_data = sample_data
-        return "chuchu", 0
+	def _init_(self):
+		self.name = "dumb"
 
-    def get_name(self):
-        return self.name
+	def classify(self, data):
+		return 0
+
+	def get_name(self):
+		return self.name
