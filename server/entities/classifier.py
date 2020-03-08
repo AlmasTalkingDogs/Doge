6229c6d83d3e104b1f296a16e3651e2ef765af01
asdhruv2 <asdhruv2@illinois.edu>
2020-03-08 13:47:17 -0500

    fixed the classifier class

--- server/entities/classifier.py
+++ server/entities/classifier.py
@@ -1,13 +1,11 @@
-from abc import ABC
+class Classifier():
 
-
-class Classifier(ABC):
-    name = "sampleName"
-    sample_data = None
-
+    def _init_(self):
+    	self.name = "sampleName"
+    	self.sample_data = None
     def classify(self, sample_data):
-        self.sample_data = sample_data
-        return 0
+    	self.sample_data = sample_data
+        return "chuchu", 0
 
     def get_name(self):
         return self.name
